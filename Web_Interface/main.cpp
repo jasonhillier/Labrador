1c0c19f5b3ff67c8f5469ad03434d167afc78550
jasonhillier <jason@hillier.us>
2022-08-16 13:32:22 -0600

    fix

--- Web_Interface/main.cpp
+++ Web_Interface/main.cpp
@@ -21,8 +21,7 @@ static void signal_handler(int signo) {
 // Simply serve static files from `s_root_dir`
 static void cb(struct mg_connection *c, int ev, void *ev_data, void *fn_data) {
   if (ev == MG_EV_HTTP_MSG) {
-    /*
-    struct mg_http_message *hm = ev_data, tmp = {0};
+    struct mg_http_message *hm = (mg_http_message*)ev_data, tmp = {0};
     struct mg_str unknown = mg_str_n("?", 1), *cl;
     struct mg_http_serve_opts opts = {0};
     opts.root_dir = s_root_dir;
@@ -34,7 +33,6 @@ static void cb(struct mg_connection *c, int ev, void *ev_data, void *fn_data) {
     MG_INFO(("%.*s %.*s %.*s %.*s", (int) hm->method.len, hm->method.ptr,
              (int) hm->uri.len, hm->uri.ptr, (int) tmp.uri.len, tmp.uri.ptr,
              (int) cl->len, cl->ptr));
-             */
   }
   (void) fn_data;
 }
